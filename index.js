b8be955cc3607958f65e172bd701a807d58efd4a
adam N <adam.nawroc@gmail.com>
2018-02-21 17:50:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
 
-  return obj
+  return object
 }
