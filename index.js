9b4e635f69748850768edbaacb176c9397570a66
adam N <adam.nawroc@gmail.com>
2018-02-21 17:50:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 
   return Object.assign({}, obj, { [key]: value })
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
